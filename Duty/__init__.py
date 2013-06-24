5aafa9ffdf0f8caffa40c4a104f58a322f854ed4
Julien Freyermuth <julien.chipster@gmail.com>
2013-06-24 22:10:02 +0200

    Adding some changes in duty and beginning.py files to respect PEP8

--- Duty/__init__.py
+++ Duty/__init__.py
@@ -1,5 +1,6 @@
-from. version import *
 from. begin import *
 from. duty_with_number import *
 from. duty_with_time import *
 from. is_a_number import *
+from. mylog import *
+from. version import *
